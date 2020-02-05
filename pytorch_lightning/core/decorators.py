10e996213e09e8fb5ddcc63f24512cedc6673515
Yiyu Zhu <yiyu@kneron.us>
2020-02-05 15:52:10 -0800

    fix __name__ when using decorator

--- pytorch_lightning/core/decorators.py
+++ pytorch_lightning/core/decorators.py
@@ -8,9 +8,9 @@ def data_loader(fn):
     :param fn:
     :return:
     """
-    wraps(fn)
     attr_name = '_lazy_' + fn.__name__
-
+    
+    @wraps(fn)
     def _get_data_loader(self):
         try:
             value = getattr(self, attr_name)
